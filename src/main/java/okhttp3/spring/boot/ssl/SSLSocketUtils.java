e7a05189a0641adb1adcf926ad45068bebbb7033
wandl <hiwepy@163.com>
2021-04-20 21:33:57 +0800

    ssl

--- src/main/java/okhttp3/spring/boot/ssl/SSLSocketUtils.java
+++ src/main/java/okhttp3/spring/boot/ssl/SSLSocketUtils.java
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package okhttp3.spring.boot.ssl;
 
 import java.lang.reflect.InvocationTargetException;
@@ -23,7 +5,7 @@ import java.lang.reflect.Method;
 
 import javax.net.ssl.SSLSocket;
 
-/**
+/*
  * General utilities for SSLSocket.
  * @since 3.4
  */
@@ -32,7 +14,7 @@ public class SSLSocketUtils {
         // Not instantiable
     }
 
-    /**
+    /*
      * Enable the HTTPS endpoint identification algorithm on an SSLSocket.
      * @param socket the SSL socket
      * @return {@code true} on success (this is only supported on Java 1.7+)
--- src/main/java/okhttp3/spring/boot/ssl/TrustManagerUtils.java
+++ src/main/java/okhttp3/spring/boot/ssl/TrustManagerUtils.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package okhttp3.spring.boot.ssl;
 
 import java.security.GeneralSecurityException;
@@ -22,97 +5,115 @@ import java.security.KeyStore;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
-/**
+/*
  * TrustManager utilities for generating TrustManagers.
- *
+ * 
  * @since 3.0
  */
-public final class TrustManagerUtils
-{
-    private static final X509Certificate[] EMPTY_X509CERTIFICATE_ARRAY = new X509Certificate[]{};
+public final class TrustManagerUtils {
 
-    private static class TrustManager implements X509TrustManager {
+	private static final X509Certificate[] EMPTY_X509CERTIFICATE_ARRAY = new X509Certificate[] {};
 
-        private final boolean checkServerValidity;
+	// 用于解决javax.net.ssl.SSLPeerUnverifiedException: peer not authenticated
+	private static class TrustManager implements X509TrustManager {
 
-        TrustManager(boolean checkServerValidity) {
-            this.checkServerValidity = checkServerValidity;
-        }
+		private final boolean checkServerValidity;
 
-        /**
-         * Never generates a CertificateException.
-         */
-        @Override
-        public void checkClientTrusted(X509Certificate[] certificates, String authType)
-        {
-            return;
-        }
+		TrustManager(boolean checkServerValidity) {
+			this.checkServerValidity = checkServerValidity;
+		}
 
-        @Override
-        public void checkServerTrusted(X509Certificate[] certificates, String authType)
-            throws CertificateException
-        {
-            if (checkServerValidity) {
-                for (X509Certificate certificate : certificates)
-                {
-                    certificate.checkValidity();
-                }
-            }
-        }
+		/*
+		 * Never generates a CertificateException.
+		 */
+		public void checkClientTrusted(X509Certificate[] certificates, String authType) {
+		}
 
-        /**
-         * @return an empty array of certificates
-         */
-        @Override
-        public X509Certificate[] getAcceptedIssuers()
-        {
-            return EMPTY_X509CERTIFICATE_ARRAY;
-        }
-    }
+		public void checkServerTrusted(X509Certificate[] certificates, String authType) throws CertificateException {
+			if (checkServerValidity) {
+				for (X509Certificate certificate : certificates) {
+					certificate.checkValidity();
+				}
+			}
+		}
 
-    private static final X509TrustManager ACCEPT_ALL=new TrustManager(false);
+		/*
+		 * @return an empty array of certificates
+		 */
+		public X509Certificate[] getAcceptedIssuers() {
+			return EMPTY_X509CERTIFICATE_ARRAY;
+		}
+	}
+	
+	private static class TrustHostnameVerifier implements HostnameVerifier {
+		
+		@Override
+		public boolean verify(String hostname, SSLSession session) {
+			return true;
+		}
+		
+	}
 
-    private static final X509TrustManager CHECK_SERVER_VALIDITY=new TrustManager(true);
+	// 创建TrustManager()
+	private static final X509TrustManager ACCEPT_ALL = new TrustManager(false);
 
-    /**
-     * Generate a TrustManager that performs no checks.
-     *
-     * @return the TrustManager
-     */
-    public static X509TrustManager getAcceptAllTrustManager(){
-        return ACCEPT_ALL;
-    }
+	private static final X509TrustManager CHECK_SERVER_VALIDITY = new TrustManager(true);
 
-    /**
-     * Generate a TrustManager that checks server certificates for validity,
-     * but otherwise performs no checks.
-     *
-     * @return the validating TrustManager
-     */
-    public static X509TrustManager getValidateServerCertificateTrustManager(){
-        return CHECK_SERVER_VALIDITY;
-    }
+	private static final HostnameVerifier ACCEPT_ALL_VERIFIER = new TrustHostnameVerifier();
+	
+	/*
+	 * Generate a HostnameVerifier that performs no checks.
+	 *
+	 * @return the HostnameVerifier
+	 */
+	public static HostnameVerifier getAcceptAllHostnameVerifier() {
+		return ACCEPT_ALL_VERIFIER;
+	}
+	
+	/*
+	 * Generate a TrustManager that performs no checks.
+	 *
+	 * @return the TrustManager
+	 */
+	public static X509TrustManager getAcceptAllTrustManager() {
+		return ACCEPT_ALL;
+	}
 
-    /**
-     * Return the default TrustManager provided by the JVM.
-     * <p>
-     * This should be the same as the default used by
-     *  {@link javax.net.ssl.SSLContext#init(javax.net.ssl.KeyManager[], javax.net.ssl.TrustManager[], java.security.SecureRandom)
-     * SSLContext#init(KeyManager[], TrustManager[], SecureRandom)}
-     * when the TrustManager parameter is set to {@code null}
-     * @param keyStore the KeyStore to use, may be {@code null}
-     * @return the default TrustManager
-     * @throws GeneralSecurityException if an error occurs
-     */
-    public static X509TrustManager getDefaultTrustManager(KeyStore keyStore) throws GeneralSecurityException {
-        String defaultAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(defaultAlgorithm);
-        trustManagerFactory.init(keyStore);
-		return (X509TrustManager) trustManagerFactory.getTrustManagers()[0];
-        
-    }
+	/*
+	 * Generate a TrustManager that checks server certificates for validity, but
+	 * otherwise performs no checks.
+	 *
+	 * @return the validating TrustManager
+	 */
+	public static X509TrustManager getValidateServerCertificateTrustManager() {
+		return CHECK_SERVER_VALIDITY;
+	}
+
+	/*
+	 * Return the default TrustManager provided by the JVM.
+	 * <p>
+	 * This should be the same as the default used by
+	 * {@link javax.net.ssl.SSLContext#init(javax.net.ssl.KeyManager[], javax.net.ssl.TrustManager[], java.security.SecureRandom)
+	 * SSLContext#init(KeyManager[], TrustManager[], SecureRandom)} when the
+	 * TrustManager parameter is set to {@code null}
+	 * 
+	 * @param keyStore the KeyStore to use, may be {@code null}
+	 * @return the default TrustManager
+	 * @throws GeneralSecurityException if an error occurs
+	 */
+	public static X509TrustManager getDefaultTrustManager(KeyStore keyStore) throws GeneralSecurityException {
+		String defaultAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+		TrustManagerFactory instance = TrustManagerFactory.getInstance(defaultAlgorithm);
+		instance.init(keyStore);
+		return (X509TrustManager) instance.getTrustManagers()[0];
+	}
+	
+	
+
 
 }
