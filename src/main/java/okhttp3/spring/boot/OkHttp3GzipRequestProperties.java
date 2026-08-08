64bc4cdcff626cd0c22d84d6ac24e10bd391474e
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:59:42 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.5.x branch

--- src/main/java/okhttp3/spring/boot/OkHttp3GzipRequestProperties.java
+++ src/main/java/okhttp3/spring/boot/OkHttp3GzipRequestProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, hiwepy (https://github.com/easy-4-java).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -20,7 +20,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
  * Http Request Gzip 配置
- * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 @ConfigurationProperties(OkHttp3GzipRequestProperties.PREFIX)
 @Data
--- src/main/java/okhttp3/spring/boot/OkHttp3MetricsProperties.java
+++ src/main/java/okhttp3/spring/boot/OkHttp3MetricsProperties.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * @author wandl
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 @ConfigurationProperties(OkHttp3MetricsProperties.PREFIX)
 @Data
