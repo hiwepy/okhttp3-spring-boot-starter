2cbf13b3267c5068fc5c6cf5ee9abbec8e80944b
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:43:55 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/okhttp3/spring/boot/OkHttp3RequestGzipProperties.java
+++ src/main/java/okhttp3/spring/boot/OkHttp3RequestGzipProperties.java
@@ -20,7 +20,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
  * Http Request Gzip 配置
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @ConfigurationProperties(OkHttp3RequestGzipProperties.PREFIX)
 @Data
